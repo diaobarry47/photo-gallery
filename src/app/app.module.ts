eafba9d5c528dc4580e4668c39249adad4731a7d
Mouhamadou Diao Barry <mouhamadoudiaobarry@gmail.com>
2019-06-14 11:32:43 +0000

    added camera functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Camera } from '@ionic-native/camera/ngx';
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
